fc7f786f4bb43efecf2d581c762e294edc93a580
Ed Elliott <ed.elliott@pearson.com>
2019-04-01 21:41:14 -0400

    Initial Commit

---
+++ android/src/main/java/com/ekeservices/realm_flutter_plugin/RealmFlutterPlugin.java
@@ -0,0 +1,25 @@
+package com.ekeservices.realm_flutter_plugin;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** RealmFlutterPlugin */
+public class RealmFlutterPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "realm_flutter_plugin");
+    channel.setMethodCallHandler(new RealmFlutterPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/RealmFlutterPlugin.m
@@ -0,0 +1,20 @@
+#import "RealmFlutterPlugin.h"
+
+@implementation RealmFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"realm_flutter_plugin"
+            binaryMessenger:[registrar messenger]];
+  RealmFlutterPlugin* instance = [[RealmFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
